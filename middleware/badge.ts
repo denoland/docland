80de20b680b9b5c0516d8458681d3e7fd48ffb61
Kitson Kelly <me@kitsonkelly.com>
2021-12-04 08:18:47 +1100

    fix: add back badge.svg (#18)
    Fixes #17

---
+++ middleware/badge.ts
@@ -0,0 +1,22 @@
+// Copyright 2021 the Deno authors. All rights reserved. MIT license.
+import type { Middleware } from "../deps.ts";
+
+export function createBadgeMW(): Middleware {
+  let badge: Uint8Array | undefined;
+  return async function favicon(ctx, next) {
+    if (ctx.request.url.pathname !== "/badge.svg") {
+      return next();
+    }
+    if (!badge) {
+      badge = await Deno.readFile(
+        new URL("../static/badge.svg", import.meta.url),
+      );
+    }
+    ctx.response.body = badge;
+    ctx.response.type = "image/svg+xml";
+    ctx.response.headers.set(
+      "expires",
+      new Date(Date.now() + 31_436_000_000).toUTCString(),
+    );
+  };
+}
