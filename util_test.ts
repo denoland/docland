a874cb460bb1c965a3d4a95df7153fb35526c0af
Kitson Kelly <me@kitsonkelly.com>
2021-12-04 20:56:14 +0000

    feat: add more registry patterns

---
+++ util_test.ts
@@ -0,0 +1,38 @@
+// Copyright 2021 the Deno authors. All rights reserved. MIT license.
+
+import { assertEquals } from "./deps_test.ts";
+import { assert, parseURL } from "./util.ts";
+
+Deno.test({
+  name: "parseURL() - github raw URLs",
+  fn() {
+    const actual = parseURL(
+      "https://github.com/denoland/deno_std/raw/main/http/mod.ts",
+    );
+    assert(actual);
+    assertEquals(actual, {
+      registery: "github.com",
+      org: "denoland",
+      package: "deno_std",
+      version: "main",
+      module: "http/mod.ts",
+    });
+  },
+});
+
+Deno.test({
+  name: "parseURL() - esm CDN urls",
+  fn() {
+    const actual = parseURL(
+      "https://cdn.esm.sh/v58/firebase@9.4.1/database/dist/database/index.d.ts",
+    );
+    assert(actual);
+    assertEquals(actual, {
+      registry: "esm.sh @ v58",
+      org: undefined,
+      package: "firebase",
+      version: "9.4.1",
+      module: "database/dist/database/index.d.ts",
+    });
+  },
+});
