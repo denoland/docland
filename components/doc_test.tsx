9ed2577d42c9a6e576e4c5ed8b4a87f91b43ea7d
Kitson Kelly <me@kitsonkelly.com>
2022-02-13 22:59:53 +0000

    feat: add import usage for individual symbols
    Closes: #103

--- components/doc_test.tsx
+++ components/doc_test.tsx
@@ -110,6 +110,19 @@ Deno.test({
         </nav>
         <article class="tw-baauk4">
           <h1 class="tw-d0anel">fn</h1>
+          <div>
+            <div class="tw-1esk77l">
+              <pre>
+                {`<button class="float-right px-2 font-sans focus-visible:ring-2 text-sm text-gray-500 dark:text-gray-300 border border-gray-300 dark:border-gray-500 rounded hover:shadow" type="button" onclick="copyImportStatement()">Copy</button>`}
+                <code>
+                  <span class="code-keyword">import</span> &#123; fn &#125;{" "}
+                  <span class="code-keyword">from</span>{" "}
+                  <span class="code-string">"https://example.com/mod.ts"</span>;
+                </code>
+              </pre>
+            </div>
+            {`<script>function copyImportStatement() {          navigator.clipboard.writeText(\`import { fn } from "https://example.com/mod.ts";\`);        }</script>`}
+          </div>
           <div class="tw-1nkr705">
             <div>
               <span class="tw-18hyoot">function{" "}</span>
@@ -180,7 +193,7 @@ Deno.test({
 });
 
 Deno.test({
-  name: "Usage component",
+  name: "Usage component - namespace import",
   fn() {
     const Expected = () => (
       <div>
@@ -203,6 +216,125 @@ Deno.test({
     );
     const actual = renderSSR(
       <Usage>https://deno.land/x/example_package/mod.ts</Usage>,
+    )
+      .replaceAll("\n", "");
+    const expected = renderSSR(<Expected />).replaceAll("\n", "");
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "Usage component - named import",
+  fn() {
+    const Expected = () => (
+      <div>
+        <div class="tw-1esk77l">
+          <pre>
+            {`<button class="float-right px-2 font-sans focus-visible:ring-2 text-sm text-gray-500 dark:text-gray-300 border border-gray-300 dark:border-gray-500 rounded hover:shadow" type="button" onclick="copyImportStatement()">Copy</button>`}
+            <code>
+              <span class="code-keyword">import</span> &#123; a &#125;{" "}
+              <span class="code-keyword">from</span>{" "}
+              <span class="code-string">
+                "https://deno.land/x/example_package/mod.ts"
+              </span>;
+            </code>
+          </pre>
+        </div>
+        {`<script>function copyImportStatement() {          navigator.clipboard.writeText(\`import { a } from "https://deno.land/x/example_package/mod.ts";\`);        }</script>`}
+      </div>
+    );
+    const actual = renderSSR(
+      <Usage item="a">https://deno.land/x/example_package/mod.ts</Usage>,
+    )
+      .replaceAll("\n", "");
+    const expected = renderSSR(<Expected />).replaceAll("\n", "");
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "Usage component - named type import",
+  fn() {
+    const Expected = () => (
+      <div>
+        <div class="tw-1esk77l">
+          <pre>
+            {`<button class="float-right px-2 font-sans focus-visible:ring-2 text-sm text-gray-500 dark:text-gray-300 border border-gray-300 dark:border-gray-500 rounded hover:shadow" type="button" onclick="copyImportStatement()">Copy</button>`}
+            <code>
+              <span class="code-keyword">import</span>{" "}
+              <span class="code-keyword">type{" "}</span>&#123; A &#125;{" "}
+              <span class="code-keyword">from</span>{" "}
+              <span class="code-string">
+                "https://deno.land/x/example_package/mod.ts"
+              </span>;
+            </code>
+          </pre>
+        </div>
+        {`<script>function copyImportStatement() {          navigator.clipboard.writeText(\`import type { A } from "https://deno.land/x/example_package/mod.ts";\`);        }</script>`}
+      </div>
+    );
+    const actual = renderSSR(
+      <Usage item="A" isType>https://deno.land/x/example_package/mod.ts</Usage>,
+    )
+      .replaceAll("\n", "");
+    const expected = renderSSR(<Expected />).replaceAll("\n", "");
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "Usage component - named namespace import",
+  fn() {
+    const Expected = () => (
+      <div>
+        <div class="tw-1esk77l">
+          <pre>
+            {`<button class="float-right px-2 font-sans focus-visible:ring-2 text-sm text-gray-500 dark:text-gray-300 border border-gray-300 dark:border-gray-500 rounded hover:shadow" type="button" onclick="copyImportStatement()">Copy</button>`}
+            <code>
+              <span class="code-keyword">import</span> &#123; a &#125;{" "}
+              <span class="code-keyword">from</span>{" "}
+              <span class="code-string">
+                "https://deno.land/x/example_package/mod.ts"
+              </span>; <span class="code-keyword">const</span>{" "}
+              &#123; b &#125; = a;
+            </code>
+          </pre>
+        </div>
+        {`<script>function copyImportStatement() {          navigator.clipboard.writeText(\`import { a } from "https://deno.land/x/example_package/mod.ts";const { b } = a;\`);        }</script>`}
+      </div>
+    );
+    const actual = renderSSR(
+      <Usage item="a.b">https://deno.land/x/example_package/mod.ts</Usage>,
+    )
+      .replaceAll("\n", "");
+    const expected = renderSSR(<Expected />).replaceAll("\n", "");
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "Usage component - deep named namespace import",
+  fn() {
+    const Expected = () => (
+      <div>
+        <div class="tw-1esk77l">
+          <pre>
+            {`<button class="float-right px-2 font-sans focus-visible:ring-2 text-sm text-gray-500 dark:text-gray-300 border border-gray-300 dark:border-gray-500 rounded hover:shadow" type="button" onclick="copyImportStatement()">Copy</button>`}
+            <code>
+              <span class="code-keyword">import</span> &#123; a &#125;{" "}
+              <span class="code-keyword">from</span>{" "}
+              <span class="code-string">
+                "https://deno.land/x/example_package/mod.ts"
+              </span>; <span class="code-keyword">const</span>{" "}
+              &#123; c &#125; = a.b;
+            </code>
+          </pre>
+        </div>
+        {`<script>function copyImportStatement() {          navigator.clipboard.writeText(\`import { a } from "https://deno.land/x/example_package/mod.ts";const { c } = a.b;\`);        }</script>`}
+      </div>
+    );
+    const actual = renderSSR(
+      <Usage item="a.b.c">https://deno.land/x/example_package/mod.ts</Usage>,
     )
       .replaceAll("\n", "");
     const expected = renderSSR(<Expected />).replaceAll("\n", "");
